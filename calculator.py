72288e62af67cf3c194c4722f7adb0d26222e3bf
balakose <balakose24@gmail.com>
2025-05-01 18:33:47 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# Calculator
+number1=float(input("firstnumber"))
+operator=input("enter operator")
+number2=float(input("secondnumber"))
+if operator=="+":
+    print(number1+number2)
+elif operator=="-":
+    print(number1-number2)
+elif operator=="*":
+    print(number1*number2)
+elif operator=="/":    
+    pint(number1/number2)
+else:
+    print("invalid operator")
+
+
